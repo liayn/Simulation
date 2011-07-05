07f338f0999d20de66b39e429e11b96731893df4
Johannes Kasberger <schugabe@gmx.at>
2011-07-05 15:10:04 +0200

    diskreter regler bei bsp1

---
+++ bsp1/koef.m
@@ -0,0 +1,25 @@
+% Koef. für Simulation
+g=9.81;
+A=0.00278;
+A12=18.54e-6;
+A23=17.5e-6;
+A34=18.75e-6;
+A4=13.44e-6;
+hmax=0.338;
+
+T_a = 1;
+
+T_n=127;
+T_v=12.7;
+K_p=5;
+
+% Koef. für Diskreten Regler
+T_1 = T_n/2 + sqrt(T_n^2/4 -T_n*T_v);
+T_2 = T_n/2 - sqrt(T_n^2/4 -T_n*T_v);
+T_p = min(T_1,T_2)*0.05;
+
+K_pid = K_p/T_n;
+
+G_par = K_pid*tf([T_1*T_2 T_1+T_2 1],[T_p 1 0]);
+    
+H = c2d(G_par,T_a,'tustin');
