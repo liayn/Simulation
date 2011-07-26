c2bce1155b8dbfba8269e550146f8b027ccb8b35
dpara <exor890@aon.at>
2011-07-26 22:35:55 +0200

    fahrrad first tries

--- bsp3/koeff.m
+++ bsp3/koeff.m
@@ -1,14 +1,25 @@
 
 g = 9.81
-v = 10
-m = 1
-d = 1
-J = 1
-Js = 1
-h = 1
+v = 3
+bm = 5           %bike mass
+dm = 50          %biker mass
+d = 0.6          %tire (diameter i guess)
+h = 0.9          %bike height
+dh = 0.5         %biker height
+
+m = bm + dm      %total mass
+J = bm*h*h       %trägheitsmoment of bike
+Js = dm*dh*dh    %trägheitsmoment of biker
 
 Ki = [1 1]
 Kp = [1 1]
 Kd = [1 1]
 
 tau = 0.2
+
+
+dmv = d*m*v
+mv2 = m * v * v
+mhg = 1 / (m*h*g)
+Jsh2m = Js + h*h*m
+h2mg = h/2*m*g       
